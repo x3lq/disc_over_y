69453991b4faa65caf49f01b2973df4882043802
Unknown <ga45jas@mytum.de>
2018-12-01 12:39:46 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ SheepDeep/Assets/Scripts/SheepCrowdControllerScr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepCrowdControllerScr : MonoBehaviour {
+
+    public GameObject boidPrefab;
+
+    public int spawnCount = 10;
+
+    public float spawnRadius = 4.0f;
+
+    [Range(0.1f, 20.0f)]
+    public float velocity = 6.0f;
+
+    [Range(0.0f, 0.9f)]
+    public float velocityVariation = 0.5f;
+
+    [Range(0.1f, 20.0f)]
+    public float rotationCoeff = 4.0f;
+
+    [Range(0.1f, 10.0f)]
+    public float neighborDist = 2.0f;
+
+    public LayerMask searchLayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
