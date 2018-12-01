ef242a9dc7b629c5be32a5e0f0363aa2dcc7b8bb
Unknown <ga45jas@mytum.de>
2018-12-01 18:34:44 +0100

    SheepMovement
    Herd movement must be improved

--- SheepDeep/Assets/Scripts/Sheep/Sheep.cs
+++ SheepDeep/Assets/Scripts/Sheep/Sheep.cs
@@ -2,23 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sheep : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class Sheep : MonoBehaviour
+{
 
     private Renderer renderer;
+    private Rigidbody2D rigidbody;
+    private SheepManager manager;
 
     public bool hasWool;
 
     public float growWoolTime;
 
-	// Use this for initialization
-	void Start () {
+    private int stepCount = 0;
+    private int noStepCount = 0;
+
+    private float movementCooldown = 0;
+
+    float noiseOffset;
+
+    public float animationSpeedVariation = 0.2f;
+
+    // Use this for initialization
+    void Start()
+    {
         renderer = GetComponent<Renderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        rigidbody = transform.GetComponent<Rigidbody2D>();
+        manager = FindObjectOfType<SheepManager>();
+
+        noiseOffset = Random.value * 10.0f;
+
+        var animator = GetComponent<Animator>();
+        if (animator)
+            animator.speed = Random.Range(-1.0f, 1.0f) * animationSpeedVariation + 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (movementCooldown <= 0)
+        {
+            //movementCooldown = 1;
+        }
+        else
+        {
+            movementCooldown -= Time.deltaTime;
+            return;
+        }
+
+        int randomDirection;
+        if (stepCount > 30)
+        {
+            randomDirection = 0;
+        }
+        else
+        {
+            randomDirection = UnityEngine.Random.Range(noStepCount > 10 ? 1 : 0, 5);
+        }
+
+        switch (randomDirection)
+        {
+            case 1:
+                //Move up
+                rigidbody.velocity = CalculateCrowdMovement(new Vector2(rigidbody.velocity.x, rigidbody.velocity.y + 0.1f));
+                stepCount++;
+                break;
+            case 2:
+                //Move right
+                rigidbody.velocity = CalculateCrowdMovement(new Vector2(rigidbody.velocity.x + 0.1f, rigidbody.velocity.y));
+                stepCount++;
+                break;
+            case 3:
+                //Move down
+                rigidbody.velocity = CalculateCrowdMovement(new Vector2(rigidbody.velocity.x, rigidbody.velocity.y - 0.1f));
+                stepCount++;
+                break;
+            case 4:
+                //Move left
+                rigidbody.velocity = CalculateCrowdMovement(new Vector2(rigidbody.velocity.x - 0.1f, rigidbody.velocity.y));
+                stepCount++;
+                break;
+            default:
+                rigidbody.velocity = new Vector2(0, 0);
+                stepCount = 0;
+                noStepCount++;
+                break;
+        }
+    }
 
     public bool SheppardInteraction()
     {
@@ -52,10 +127,87 @@ public class Sheep : MonoBehaviour {
         Destroy(gameObject);
     }
 
-    public void GiveBirth(Vector3 position) {
+    public void GiveBirth(Vector3 position)
+    {
         GameObject newBorn = Instantiate(SheepManager.GetManager().sheepPrefab, position, Quaternion.identity);
         newBorn.transform.parent = SheepManager.GetManager().herd.transform;
         SheepManager.GetManager().sheeps.Add(newBorn);
         Debug.Log("New Sheep");
+    }
+
+    private void checkTarget()
+    {
+        if(manager.getTargetPosition() != null)
+        {
+            rigidbody.velocity /= 2;
+            rigidbody.velocity += ((Vector2)transform.position - manager.getTargetPosition()).normalized * 2;
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //          Helper-Methods
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    private Vector2 GetDistanceVector(Transform target)
+    {
+        Vector2 distanceVector = transform.position - target.transform.position;
+        float distance = distanceVector.magnitude;
+        float scale = Mathf.Clamp01(1.0f - distance / manager.neighborDist);
+        return distanceVector * (scale / distance);
+    }
+
+    private Vector2 CalculateCrowdVelocity()
+    {
+        float noise = Mathf.PerlinNoise(Time.time, noiseOffset) * 2.0f - 1.0f;
+        float directionVelocity = manager.velocity * (1 + noise * manager.velocityVariation);
+
+        Vector2 distance = Vector2.zero;
+        Vector2 direction = manager.transform.forward;
+        Vector2 anchor = manager.transform.position;
+
+        Collider[] neighbors = Physics.OverlapSphere(transform.position, manager.neighborDist);
+
+        foreach (Collider col in neighbors)
+        {
+            if (col.gameObject == gameObject || !col.tag.Equals("Sheep")) continue;
+            Transform trans = col.transform;
+            distance += GetDistanceVector(trans);
+            direction += (Vector2)trans.forward;
+            anchor += (Vector2)trans.position;
+        }
+
+        direction *= 1.0f / neighbors.Length;
+        anchor *= 1.0f / neighbors.Length;
+        anchor = (anchor - (Vector2)transform.position).normalized;
+
+        Vector3 lookDirektion = distance + direction + anchor;
+
+        return lookDirektion.normalized;
+    }
+
+    private Vector2 CalculateCrowdMovement(Vector2 ownVelocity)
+    {
+        Collider[] neighbors = Physics.OverlapSphere(transform.position, manager.neighborDist);
+
+        if (neighbors.Length <= 1) return ownVelocity;
+        int sheepCounter = 0;
+
+        ownVelocity *= 2;
+
+        foreach (Collider col in neighbors)
+        {
+            if (col.gameObject == gameObject || !col.tag.Equals("Sheep")) continue;
+            GameObject obj = col.gameObject;
+            sheepCounter++;
+            ownVelocity += obj.GetComponent<Sheep>().GetVelocity();
+        }
+        ownVelocity *= 1.0f / (sheepCounter + 2);
+        Debug.Log(ownVelocity);
+        return ownVelocity;
+    }
+
+    public Vector2 GetVelocity()
+    {
+        return rigidbody.velocity;
     }
 }
--- SheepDeep/Assets/Scripts/Sheep/SheepManager.cs
+++ SheepDeep/Assets/Scripts/Sheep/SheepManager.cs
@@ -11,6 +11,7 @@ public class SheepManager : MonoBehaviour {
 	public GameObject sheepPrefab; 
 	public GameObject herd;
 
+    [Range(0, 0.9f)]
 	public float distanceToBorder;
 	public int amount;
 
@@ -18,7 +19,23 @@ public class SheepManager : MonoBehaviour {
 	public float randomSpawnVariance;
 	private float spawnTimer;
 
-	void Awake()
+    #region crowdMovementVariables
+    [Range(0.1f, 20.0f)]
+    public float velocity = 6.0f;
+
+    [Range(0.0f, 0.9f)]
+    public float velocityVariation = 0.5f;
+
+    [Range(0.1f, 20.0f)]
+    public float rotationCoeff = 4.0f;
+
+    [Range(0.1f, 10.0f)]
+    public float neighborDist = 2.0f;
+
+    private Vector2 targetPosition = Vector2.zero;
+    #endregion
+
+    void Awake()
 	{
 		if(instance == null){
 			instance = this;		
@@ -26,8 +43,8 @@ public class SheepManager : MonoBehaviour {
 			Destroy(this);
 		}
 	}
-	void Start () {
 
+	void Start () {
 		sheeps = new List<GameObject>();
 		for(int i=0; i<amount; i++) {
 			GameObject sheep = GameObject.Instantiate(sheepPrefab, new Vector3(randomCoordinate(x_maxSize),
@@ -38,18 +55,23 @@ public class SheepManager : MonoBehaviour {
 	}
 
 	void Update() {
-
 		if(spawnTimer > 0) {
 			spawnTimer -= Time.deltaTime;
 		}else {
 			spawnTimer = randomSpawnMedian + Random.Range(-1, 1) * randomSpawnVariance * randomSpawnMedian;
-			GameObject randomSheep = sheeps[Random.Range(0, sheeps.Count)];
+			/*GameObject randomSheep = sheeps[Random.Range(0, sheeps.Count)];
 			Sheep sheep = randomSheep.GetComponent("Sheep") as Sheep;
 
 			if(sheep != null) {
 				sheep.GiveBirth(randomSheep.transform.position);
-			}
+			}*/
 		}
+
+        int movementMotivation = Random.Range(0, 20);
+        if(movementMotivation == 19)
+        {
+            chooseNewTarget();
+        }
 	}
 
 	private int randomCoordinate(int maxSize) {
@@ -59,4 +81,21 @@ public class SheepManager : MonoBehaviour {
 	public static SheepManager GetManager() {
 		return instance;
 	}
+
+    public Vector2 getTargetPosition()
+    {
+        return targetPosition;
+    }
+
+    private void chooseNewTarget()
+    {
+        targetPosition = new Vector2(Random.Range(-x_maxSize, x_maxSize + 1), Random.Range(-y_maxSize, y_maxSize + 1));
+        StartCoroutine("DeleteSheepTarget");
+    }
+
+    IEnumerator DeleteSheepTarget()
+    {
+        yield return new WaitForSeconds(15f);
+        targetPosition = Vector2.zero;
+    }
 }
