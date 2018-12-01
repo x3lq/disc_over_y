5970c73b0574bd60c714a517a6c429403fcedd89
Florian Sprang <florian.sprang@tum.de>
2018-12-01 09:50:37 +0100

    first menu scenes

---
+++ SheepDeep/Assets/Scripts/Menu/WarningIntro.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarningIntro : MonoBehaviour {
+
+
+	public void onAnimationEnd() {
+		SceneManager.LoadScene(1);
+	}
+}
