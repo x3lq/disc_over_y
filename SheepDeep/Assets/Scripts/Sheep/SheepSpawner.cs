6be9909ed38c0417e101d17fd3643c18e5b5654a
Florian Sprang <florian.sprang@tum.de>
2018-12-01 05:15:55 +0100

    adding sheep spawner

---
+++ SheepDeep/Assets/Scripts/Sheep/SheepSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepSpawner : MonoBehaviour {
+
+	public static ArrayList sheeps;
+	//anchor point is bottom left
+	public int x_maxSize, y_maxSize;
+	public GameObject sheepPrefab; 
+	public GameObject herd;
+
+	public float distanceToBorder;
+	public int amount;
+
+	// Use this for initialization
+	void Start () {
+		sheeps = new ArrayList();
+
+		for(int i=0; i<amount; i++) {
+			GameObject sheep = GameObject.Instantiate(sheepPrefab, new Vector3(randomCoordinate(x_maxSize), randomCoordinate(y_maxSize),0), Quaternion.identity);
+			sheep.transform.parent = herd.transform;
+		}
+	}
+
+	public static ArrayList getSheeps() {
+		return sheeps;
+	}
+
+	private int randomCoordinate(int maxSize) {
+		return Random.Range((0 + (int) (maxSize*distanceToBorder)), maxSize - (int) (maxSize*distanceToBorder));
+	}
+}
