d180fa7ff27b2b4c3289cd3e16e340782a2cd7a6
Robin Ostner <Ostner_Robin@web.de>
2018-12-02 08:26:52 +0100

    Added Label

--- SheepDeep/Assets/GameUI.cs
+++ SheepDeep/Assets/GameUI.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour {
 
-    public RectTransform label;
+    public RectTransform shepherdsLabel;
+    public RectTransform wolfsLabel;
+
+    public Image wolfIcon;
+    public Image shepherdsIcon;
 
     public float size = 4;
     public float percentage;
+    public int fakeCount;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +23,23 @@ public class GameUI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         int sheepCount = SheepManager.GetManager().sheeps.Count;
-        
-        label.pivot = new Vector2(percentage * size * 2 - size + 0.5f, 0.5f);
+        int wolfWin = GameLoop.getInstance().wolfWin;
+        int shepherdWin = GameLoop.getInstance().shepartWin;
+
+        percentage = ((float)(sheepCount - wolfWin)) / (shepherdWin - wolfWin);
+
+        if(percentage < 0.5f)
+        {
+            wolfIcon.enabled = true;
+            shepherdsIcon.enabled = false;
+        }
+        else
+        {
+            wolfIcon.enabled = false;
+            shepherdsIcon.enabled = true;
+        }
+
+        shepherdsLabel.pivot = new Vector2(percentage * size * 2 - size + 0.5f, 0.5f);
+        wolfsLabel.pivot = new Vector2(percentage * size * 2 - size + 0.5f, 0.5f);
     }
 }
