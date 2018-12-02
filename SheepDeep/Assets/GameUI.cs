bbf629d3827e99a943609dcdaa1c64622325d912
Robin Ostner <Ostner_Robin@web.de>
2018-12-02 08:06:49 +0100

    Added Progress Bar

---
+++ SheepDeep/Assets/GameUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour {
+
+    public RectTransform label;
+
+    public float size = 4;
+    public float percentage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int sheepCount = SheepManager.GetManager().sheeps.Count;
+        
+        label.pivot = new Vector2(percentage * size * 2 - size + 0.5f, 0.5f);
+    }
+}
